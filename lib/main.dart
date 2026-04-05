581c17a9680d08f0d4a5f14e91d09ff44f4d87ac
siamahmed0011 <ahmedsiam01608@gmail.com>
2026-04-06 01:58:28 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food Waste App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Firebase Connected'),
+        ),
+        body: const Center(
+          child: Text('Firebase setup successful 🚀'),
+        ),
+      ),
+    );
+  }
+}
