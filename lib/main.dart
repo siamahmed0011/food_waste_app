7de898f0e79a265651f7c01dbd567d9257976b56
nihaljubayer <nihaljubayer777@gmail.com>
2026-04-19 00:14:50 +0600

    nihal

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
